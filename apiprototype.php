14a26e99879acfc884a60a30e685af934de9863f
samaniegolouie <48710475+samaniegolouie@users.noreply.github.com>
2024-06-03 14:49:37 +0800

    WIP 6-3-2024

---
+++ apiprototype.php
@@ -0,0 +1,40 @@
+<?php
+
+// Database configuration
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "05022024_db_rates4";
+
+// Create a connection to MySQL database
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Set headers to allow cross-origin requests
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// Handle HTTP GET request to fetch all users
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $sql = "SELECT * FROM tbl_locations";
+    $result = $conn->query($sql);
+
+    $users = array();
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $users[] = $row;
+        }
+        echo json_encode($users);
+    } else {
+        echo json_encode(array('message' => 'No users found'));
+    }
+}
+
+// Close the database connection
+$conn->close();
